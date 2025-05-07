70cd4d57f192b9773a41237aaa7fdc55ffa53aa5
mkviniciuz <mv.vinicius107@gmail.com>
2025-05-07 03:58:23 -0300

    Gerador de senhas sem repetições

--- randomlib.py
+++ randomlib.py
@@ -136,3 +136,14 @@ def varios_caminhos():
             caminhos[i] += direcao
 
             caminhos[i] = max(0, min(caminhos[i], largura-1))
+
+def gerador_senhas_sem_repeticoes(x):
+
+    caracteres = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                  '@','!','#','$','%','&','*','(',')','_','+','1','2','3','4','5','6','7','8','9','0']
+
+    senha = random.sample(caracteres, x)
+
+    print("".join(senha))
+
